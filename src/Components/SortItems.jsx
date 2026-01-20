7ea4bc1f6828f84f471cfbcb7ce3ccc7786e17c1
VanshRathor20 <vanshrathor5887@gmail.com>
2026-01-20 13:35:22 +0530

    sort desing

--- src/Components/SortItems.jsx
+++ src/Components/SortItems.jsx
@@ -11,17 +11,13 @@ const SortItems = ({ onSortChange }) => {
   };
 
   return (
-    <div className="w-full max-w-sm">
-      <div className="bg-linear-to-r from-blue-50 to-blue-100 border-2  rounded-xl p-6 shadow-lg">
-        <h3 className="text-lg font-bold text-gray-800 mb-4 flex items-center">
-          <HiMiniArrowsUpDown className="text-2xl mr-2" />
-          
-        </h3>
-
+    <div className="w-full">
+      <div className="flex items-center gap-2">
+        <HiMiniArrowsUpDown className="text-2xl text-gray-600" />
         <select
           value={sortOption}
           onChange={handleSortChange}
-          className="w-full px-4 py-3 border-2 border-blue-400 rounded-lg text-sm font-medium text-gray-700 bg-white cursor-pointer focus:outline-none "
+          className="px-3 py-2 border-2 border-blue-400 rounded-lg text-sm font-medium text-gray-700 bg-white cursor-pointer focus:outline-none focus:ring-blue-500"
         >
           <option value="default">Sort By (Default)</option>
           <option value="name-asc">Product Name (A-Z)</option>
